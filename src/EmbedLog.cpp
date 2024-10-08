4a02c4a0bb3d565a05eadb97bef6d9ad80452408
Joe Inman <joe.inman8@gmail.com>
2024-10-08 11:40:47 +0100

    Added documentation

--- src/EmbedLog.cpp
+++ src/EmbedLog.cpp
@@ -1,3 +1,32 @@
+/*
+ * EmbedLog - A Minimal Logging Library for Embedded Systems
+ *
+ * Copyright (C) 2023 Joe Inman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License as published by
+ * the Open Source Initiative.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MIT License for more details.
+ *
+ * You should have received a copy of the MIT License along with this program.
+ * If not, see <https://opensource.org/licenses/MIT>.
+ *
+ * Author: Joe Inman
+ * Email: joe.inman8@gmail.com
+ * Version: 1.0
+ *
+ * Description:
+ * EmbedLog is designed to provide a lightweight and flexible logging system 
+ * for embedded environments. It supports multiple log levels, allows 
+ * user-defined output mechanisms, and includes timestamping based on 
+ * microsecond-resolution functions.
+ *
+ */
+
 #include "EmbedLog/EmbedLog.hpp"
 
 namespace EmbedLog
@@ -81,4 +110,5 @@ namespace EmbedLog
         
         return std::string(timestamp);
     }
-}
+
+} // namespace EmbedLog
