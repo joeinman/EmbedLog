c94fa1b0f7a40bd2863c7bbda68002cda911ef2e
Joe Inman <110304373+joerossrobotics@users.noreply.github.com>
2025-04-14 14:04:10 +0100

    Made logLevelToString() static to avoid compilation issues (#3)

--- include/EmbedLog/Types.hpp
+++ include/EmbedLog/Types.hpp
@@ -51,7 +51,7 @@ enum class LogLevel : uint8_t
  * @param level The LogLevel to be converted.
  * @return A string representing the log level, with ANSI color codes for formatting.
  */
-std::string logLevelToString(LogLevel level)
+static std::string logLevelToString(LogLevel level)
 {
     switch (level)
     {
